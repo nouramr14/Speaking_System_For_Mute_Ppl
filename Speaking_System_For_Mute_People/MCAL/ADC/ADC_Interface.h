2595705b287cb4d59c43a469c29a27e24363abae
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-08-28 05:09:32 +0300

    Fix ADC interface and channel definitions

--- Speaking_System_For_Mute_People/MCAL/ADC/ADC_Interface.h
+++ Speaking_System_For_Mute_People/MCAL/ADC/ADC_Interface.h
@@ -1,60 +1,45 @@
 /*
  * ADC_Interface.h
-
  *
- *  Created on: Aug 27, 2026
- *      Author: DELL
+ * ATmega32 ADC driver interface.
  */
-
-
 #ifndef MCAL_ADC_ADC_INTERFACE_H_
 #define MCAL_ADC_ADC_INTERFACE_H_
-#include "ADC_Private.h"
-#include"ADC_Config.h"
-#include<stdint.h>
-/*modes to use with ADC_TRIGGERING_MODE*/
-#define AUTO_TRIGGER	  0
-#define SOFTWARE_TRIGGER  1
 
-/*modes to use with ADC_READING_NO_OF_BITS*/
-#define _8_BIT_MODE	  0
-#define _16_BIT_MODE  1
+#include "../../LIB/STD_Types.h"
+#include <stdint.h>
 
-/*modes to use with ADC_READING_TECHNIQUE*/
-#define INTERRUPT_MODE	0
-#define BOOLING_MODE	1
+#define AUTO_TRIGGER       0
+#define SOFTWARE_TRIGGER   1
 
-/*adcs to use with the adc_pin param*/
-#define ADC_0	(u8)0
-#define ADC_1	(u81
-#define ADC_2	(u8)2
-#define ADC_3	(u8)3
-#define ADC_4	(u8)4
-#define ADC_5	(u8)5
-#define ADC_6	(u8)6
-#define ADC_7	(u8)7
-#define ADC_ALL (u8)8
+#define _8_BIT_MODE        0
+#define _16_BIT_MODE       1
 
-/*modes to use with set auto trigger mode param*/
-#define INT_0		(u8)1
+#define INTERRUPT_MODE     0
+#define BOOLING_MODE       1
 
-/*adcs to use with the voltage_reference param*/
-#define INTERNAL_2_56_	(u8)0
-#define VCC	(uint8_t)1
-#define AREF_VOLTAGE	(u8)2
+#define ADC_0              ((u8)0)
+#define ADC_1              ((u8)1)
+#define ADC_2              ((u8)2)
+#define ADC_3              ((u8)3)
+#define ADC_4              ((u8)4)
+#define ADC_5              ((u8)5)
+#define ADC_6              ((u8)6)
+#define ADC_7              ((u8)7)
+#define ADC_ALL             ((u8)8)
 
-void ADC_vidAdcInit(uint8_t voltage_reference);
+#define INT_0              ((u8)1)
 
-void ADC_vidAdcPinInit(uint8_t adc_pin);
+#define INTERNAL_2_56_     ((u8)0)
+#define VCC                 ((u8)1)
+#define AREF_VOLTAGE        ((u8)2)
 
-void ADC_vidAdcGetRead(uint8_t adc_pin);
-
-void ADC_vidGetAutoTriggerRead();
-
+void ADC_vidAdcInit(u8 voltage_reference);
+void ADC_vidAdcPinInit(u8 adc_pin);
+void ADC_vidAdcGetRead(u8 adc_pin);
+void ADC_vidGetAutoTriggerRead(void);
 void ADC_VIDSetAutoTriggerMode(u8 mode);
-
-void ADC_vidSetReadingVariable(u8* adc_read);
-
-void ADC_vidSetCallBack( void (*func)(void));
+void ADC_vidSetReadingVariable(u16* adc_read);
+void ADC_vidSetCallBack(void (*func)(void));
 
 #endif /* MCAL_ADC_ADC_INTERFACE_H_ */
