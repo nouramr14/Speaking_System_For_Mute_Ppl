658cbd13a1c9bfebb1f1d5fe31c4e87a88db232c
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-08-28 06:03:21 +0300

    Clean flex sensor driver types and null handling

--- Speaking_System_For_Mute_People/HAL/FLEX_SENSOR/FLEX_SENSOR_Program.c
+++ Speaking_System_For_Mute_People/HAL/FLEX_SENSOR/FLEX_SENSOR_Program.c
@@ -1,31 +1,22 @@
-/*
- * FLEX_SENSOR_Program.c
- *
- *  Created on: Aug 28, 2026
- *      Author: DELL
- */
-
-#include"../../LIB/BIT_MATH.h"
-#include"../../LIB/STD_Types.h"
-#include"../../MCAL/ADC/ADC_Interface.h"
-#include"FLEX_SENSOR_Interface.h"
-
+/* FLEX_SENSOR_Program.c */
+#include "../../LIB/STD_Types.h"
+#include "../../MCAL/ADC/ADC_Interface.h"
+#include "FLEX_SENSOR_Interface.h"
 
 void FLEX_vidFlexInit(u8 sensor_pin)
 {
-
-	ADC_vidAdcInit(AREF_VOLTAGE);
-	ADC_vidAdcPinInit(sensor_pin);
-
+    ADC_vidAdcInit(AREF_VOLTAGE);
+    ADC_vidAdcPinInit(sensor_pin);
 }
 
-void FLEX_vidGetFlexRead(u16* sensor_read, uint8_t sensor_pin)
+void FLEX_vidGetFlexRead(u16* sensor_read, u8 sensor_pin)
 {
-	ADC_vidSetReadingVariable(sensor_read);
-	ADC_vidAdcGetRead(sensor_pin);
+    if (sensor_read == NULL) return;
+    ADC_vidSetReadingVariable(sensor_read);
+    ADC_vidAdcGetRead(sensor_pin);
 }
 
 void FLEX_vidSetCallBack(void (*func)(void))
 {
-	ADC_vidSetCallBack(func);
+    ADC_vidSetCallBack(func);
 }
