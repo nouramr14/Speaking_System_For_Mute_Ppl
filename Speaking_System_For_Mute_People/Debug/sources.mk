8c043bb728d6be0fee37278fea59ac29594b1a2f
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-08-28 05:11:17 +0300

    Add calibration and SHOW services to build

--- Speaking_System_For_Mute_People/Debug/sources.mk
+++ Speaking_System_For_Mute_People/Debug/sources.mk
@@ -1,23 +1,22 @@
 ################################################################################
-# Automatically-generated file. Do not edit!
+# Source directories used by the AVR build
 ################################################################################
 
-OBJ_SRCS := 
-S_SRCS := 
-ASM_SRCS := 
-C_SRCS := 
-S_UPPER_SRCS := 
-O_SRCS := 
-ELFS := 
-OBJS := 
-ASM_DEPS := 
-S_DEPS := 
-SIZEDUMMY := 
-S_UPPER_DEPS := 
-LSS := 
-C_DEPS := 
+OBJ_SRCS :=
+S_SRCS :=
+ASM_SRCS :=
+C_SRCS :=
+S_UPPER_SRCS :=
+O_SRCS :=
+ELFS :=
+OBJS :=
+ASM_DEPS :=
+S_DEPS :=
+SIZEDUMMY :=
+S_UPPER_DEPS :=
+LSS :=
+C_DEPS :=
 
-# Every subdirectory with source files must be described here
 SUBDIRS := \
 APP \
 HAL/FLEX_SENSOR \
@@ -29,8 +28,9 @@ HAL/TILT_SENSOR \
 MCAL/ADC \
 MCAL/DIO \
 MCAL/GIE \
+MCAL/SERVICES/CALIBRATION \
 MCAL/SERVICES/GET_WORD_SOUND \
 MCAL/SERVICES/GLOVE_HANDLER \
+MCAL/SERVICES/SHOW \
 MCAL/TIMER \
 MCAL/UART \
-
