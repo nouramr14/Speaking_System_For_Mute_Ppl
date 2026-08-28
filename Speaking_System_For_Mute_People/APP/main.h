02898173809fc2975b6503a05ea40b3e8ae61663
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-08-28 05:09:27 +0300

    Fix application configuration

--- Speaking_System_For_Mute_People/APP/main.h
+++ Speaking_System_For_Mute_People/APP/main.h
@@ -1,16 +1,27 @@
 /*
  * main.h
  *
- *  Created on: Aug 28, 2026
- *      Author: DELL
+ * Application configuration
  */
-
 #ifndef APP_MAIN_H_
 #define APP_MAIN_H_
 
-#define TRAIN  		0
+#define TRAIN       0
 #define IMPLEMENT   1
 
-#define APP_MODE  IMPLEMENT
+/* Normal operation. Change to TRAIN only when collecting/calibrating sensor data. */
+#define APP_MODE IMPLEMENT
+
+/*
+ * If set to 1, the application performs a new calibration at startup.
+ * Hold the STOPPING gesture while the LCD says CALIBRATE.
+ */
+#define FORCE_CALIBRATION 0
+
+/* Number of words that can be collected before playback. */
+#define MAX_WORDS_IN_SENTENCE 5
+
+/* Time between two sensor samples in milliseconds. */
+#define GESTURE_SAMPLE_DELAY_MS 250
 
 #endif /* APP_MAIN_H_ */
