5685bc25bdd59fbcbc52754cbd93d901d45e9105
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-08-28 12:52:05 +0300

    Configure Timer1 for CTC mode with /64 prescaler

--- Speaking_System_For_Mute_People/MCAL/TIMER/TIMER_Config.h
+++ Speaking_System_For_Mute_People/MCAL/TIMER/TIMER_Config.h
@@ -1,13 +1,8 @@
-/*
- * TIMER_Config.h
- *
- *  Created on: Aug 27, 2026
- *      Author: DELL
- */
-
 #ifndef MCAL_TIMER_TIMER_CONFIG_H_
 #define MCAL_TIMER_TIMER_CONFIG_H_
 
-
+/* Timer1 configuration used by the application. */
+#define TIMER1_MODE CTC_T_MODE
+#define TIMER1_PRESCALER CLK_64
 
 #endif /* MCAL_TIMER_TIMER_CONFIG_H_ */
