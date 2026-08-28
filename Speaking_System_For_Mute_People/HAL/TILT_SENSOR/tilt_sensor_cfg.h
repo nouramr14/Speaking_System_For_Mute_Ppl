a155817a0ff128a6ebe2b0bc7ff15ab77c6ce81d
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-08-28 05:12:34 +0300

    Fix tilt sensor pin table size

--- Speaking_System_For_Mute_People/HAL/TILT_SENSOR/tilt_sensor_cfg.h
+++ Speaking_System_For_Mute_People/HAL/TILT_SENSOR/tilt_sensor_cfg.h
@@ -1,22 +1,19 @@
 /*
  * tilt_sensor_cfg.h
- *
- *  Created on: Aug 27, 2026
- *      Author: DELL
  */
-
 #ifndef HAL_TILT_SENSOR_TILT_SENSOR_CFG_H_
-#define HAL_TILT_SENSOR_TILT_SENSOR_CFG_H_
-#define NO_OF_SENSORS ((uint8_t)2)
+#define HAL_TILT_SENSOR_TILT_SENSOR_CFG_H
 
-#define TILT_1_PIN	{PORT_D,PIN_0}
-#define TILT_2_PIN	{PORT_D,PIN_1}
-#define TILT_3_PIN	{PORT_D,PIN_2}
-#define TILT_4_PIN	{PORT_D,PIN_3}
-#define TILT_5_PIN	{PORT_D,PIN_4}
-#define TILT_6_PIN	{PORT_D,PIN_5}
-#define TILT_7_PIN	{PORT_D,PIN_6}
-#define TILT_8_PIN	{PORT_D,PIN_7}
+/* Eight physical positions are defined; this project uses TILT_2 and TILT_3. */
+#define NO_OF_SENSORS ((uint8_t)8)
 
+#define TILT_1_PIN {PORT_D, PIN_0}
+#define TILT_2_PIN {PORT_D, PIN_1}
+#define TILT_3_PIN {PORT_D, PIN_2}
+#define TILT_4_PIN {PORT_D, PIN_3}
+#define TILT_5_PIN {PORT_D, PIN_4}
+#define TILT_6_PIN {PORT_D, PIN_5}
+#define TILT_7_PIN {PORT_D, PIN_6}
+#define TILT_8_PIN {PORT_D, PIN_7}
 
 #endif /* HAL_TILT_SENSOR_TILT_SENSOR_CFG_H_ */
