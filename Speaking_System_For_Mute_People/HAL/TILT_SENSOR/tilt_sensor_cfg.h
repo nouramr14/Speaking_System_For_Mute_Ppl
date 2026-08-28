a0f001da3f18754cded8e05cf87b700c8e7d29eb
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-08-28 05:41:48 +0300

    Fix tilt sensor array size configuration

--- Speaking_System_For_Mute_People/HAL/TILT_SENSOR/tilt_sensor_cfg.h
+++ Speaking_System_For_Mute_People/HAL/TILT_SENSOR/tilt_sensor_cfg.h
@@ -1,19 +1,16 @@
-/*
- * tilt_sensor_cfg.h
- */
 #ifndef HAL_TILT_SENSOR_TILT_SENSOR_CFG_H_
-#define HAL_TILT_SENSOR_TILT_SENSOR_CFG_H
+#define HAL_TILT_SENSOR_TILT_SENSOR_CFG_H_
 
-/* Eight physical positions are defined; this project uses TILT_2 and TILT_3. */
-#define NO_OF_SENSORS ((uint8_t)8)
+/* Eight possible digital tilt inputs are defined by the driver. */
+#define NO_OF_SENSORS ((u8)8)
 
-#define TILT_1_PIN {PORT_D, PIN_0}
-#define TILT_2_PIN {PORT_D, PIN_1}
-#define TILT_3_PIN {PORT_D, PIN_2}
-#define TILT_4_PIN {PORT_D, PIN_3}
-#define TILT_5_PIN {PORT_D, PIN_4}
-#define TILT_6_PIN {PORT_D, PIN_5}
-#define TILT_7_PIN {PORT_D, PIN_6}
-#define TILT_8_PIN {PORT_D, PIN_7}
+#define TILT_1_PIN {PORT_D,PIN_0}
+#define TILT_2_PIN {PORT_D,PIN_1}
+#define TILT_3_PIN {PORT_D,PIN_2}
+#define TILT_4_PIN {PORT_D,PIN_3}
+#define TILT_5_PIN {PORT_D,PIN_4}
+#define TILT_6_PIN {PORT_D,PIN_5}
+#define TILT_7_PIN {PORT_D,PIN_6}
+#define TILT_8_PIN {PORT_D,PIN_7}
 
-#endif /* HAL_TILT_SENSOR_TILT_SENSOR_CFG_H_ */
+#endif
