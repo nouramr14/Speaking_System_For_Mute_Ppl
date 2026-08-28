d75da405676c71dc7bccfb22d9fe059c25f98af0
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-08-28 05:43:18 +0300

    Harden tilt sensor driver bounds and null checks

--- Speaking_System_For_Mute_People/HAL/TILT_SENSOR/tilt_sensor_prog.c
+++ Speaking_System_For_Mute_People/HAL/TILT_SENSOR/tilt_sensor_prog.c
@@ -1,47 +1,45 @@
-/*
- * tilt_sensor_prog.c
- *
- *  Created on: Aug 27, 2026
- *      Author: DELL
- */
-
-#include"../../LIB/STD_Types.h"
+#include "../../LIB/STD_Types.h"
 #include "../../MCAL/DIO/DIO_Interface.h"
 #include "tilt_sensor_cfg.h"
 #include "tilt_sensor_int.h"
 
-typedef struct{
-	uint8_t port;
-	uint8_t pin;
+typedef struct
+{
+    u8 port;
+    u8 pin;
 } TILT_SENSOR_PIN;
 
-TILT_SENSOR_PIN tilt_pins[NO_OF_SENSORS] ={TILT_1_PIN,TILT_2_PIN,TILT_3_PIN,TILT_4_PIN,
-										TILT_5_PIN,TILT_6_PIN,TILT_7_PIN,TILT_8_PIN};
-
-
-void TILT_vidInit(uint8_t tilt_pin)
+static const TILT_SENSOR_PIN tilt_pins[NO_OF_SENSORS] =
 {
-	if(tilt_pin<8)DIO_u8SetPinMode(tilt_pins[tilt_pin].port,tilt_pins[tilt_pin].pin,INPUT);
-	else if(tilt_pin==8)
-	{
-		for(uint8_t i=0;i<8;i++)
-		{
-			DIO_u8SetPinMode(tilt_pins[i].port,tilt_pins[i].pin,INPUT);
-		}
-	}
+    TILT_1_PIN,TILT_2_PIN,TILT_3_PIN,TILT_4_PIN,
+    TILT_5_PIN,TILT_6_PIN,TILT_7_PIN,TILT_8_PIN
+};
+
+void TILT_vidInit(u8 tilt_pin)
+{
+    u8 i;
+    if (tilt_pin < NO_OF_SENSORS)
+    {
+        DIO_u8SetPinMode(tilt_pins[tilt_pin].port, tilt_pins[tilt_pin].pin, INPUT);
+    }
+    else if (tilt_pin == ALL_TILT)
+    {
+        for (i = 0u; i < NO_OF_SENSORS; i++)
+            DIO_u8SetPinMode(tilt_pins[i].port, tilt_pins[i].pin, INPUT);
+    }
 }
 
-void TILT_vidGetTiltRead(uint8_t * tilt_read, uint8_t tilt_pin)
+void TILT_vidGetTiltRead(u8* tilt_read, u8 tilt_pin)
 {
-	DIO_u8GetPinValue(tilt_pins[tilt_pin].port,tilt_pins[tilt_pin].pin,tilt_read);
+    if ((tilt_read == NULL) || (tilt_pin >= NO_OF_SENSORS)) return;
+    DIO_u8GetPinValue(tilt_pins[tilt_pin].port, tilt_pins[tilt_pin].pin, tilt_read);
 }
 
-void TILT_vidGetAllTiltRead(uint8_t * tilt_read)
+void TILT_vidGetAllTiltRead(u8* tilt_read)
 {
-	tilt_read[NO_OF_SENSORS]=0;
-	for(uint8_t i=0; i<NO_OF_SENSORS;i++)
-	{
-		DIO_u8GetPinValue(tilt_pins[i].port,tilt_pins[i].pin,&(tilt_read[i]));
-	}
-	tilt_read[NO_OF_SENSORS]=1;
+    u8 i;
+    if (tilt_read == NULL) return;
+
+    for (i = 0u; i < NO_OF_SENSORS; i++)
+        DIO_u8GetPinValue(tilt_pins[i].port, tilt_pins[i].pin, &tilt_read[i]);
 }
