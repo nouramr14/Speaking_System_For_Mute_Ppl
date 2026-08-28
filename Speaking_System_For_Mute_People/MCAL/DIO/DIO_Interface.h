02b3018fc035f9ddad93634c8efe5a0f4faf1d1a
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-08-28 06:02:56 +0300

    Fix Linux case-sensitive BIT_MATH include and DIO prototypes

--- Speaking_System_For_Mute_People/MCAL/DIO/DIO_Interface.h
+++ Speaking_System_For_Mute_People/MCAL/DIO/DIO_Interface.h
@@ -1,70 +1,44 @@
-/*
- * DIO_Interface.h
- *
- *  Created on: Aug 26, 2026
- *      Author: DELL
- */
-
+/* DIO_Interface.h */
 #ifndef MCAL_DIO_DIO_INTERFACE_H_
 #define MCAL_DIO_DIO_INTERFACE_H_
+
 #include "DIO_Config.h"
 #include "DIO_Private.h"
 #include "../../LIB/STD_Types.h"
-#include "../../LIB/BIT_Math.h"
+#include "../../LIB/BIT_MATH.h"
 
-/*
- * macros to be used in port_id param
- */
-#define PORT_A (u8)0
-#define PORT_B (u8)1
-#define PORT_C (u8)2
-#define PORT_D (u8)3
+#define PORT_A ((u8)0)
+#define PORT_B ((u8)1)
+#define PORT_C ((u8)2)
+#define PORT_D ((u8)3)
 
-/*
- * macros to be used with pin_no param
- */
-#define PIN_0 (u8)0
-#define PIN_1 (u8)1
-#define PIN_2 (u8)2
-#define PIN_3 (u8)3
-#define PIN_4 (u8)4
-#define PIN_5 (u8)5
-#define PIN_6 (u8)6
-#define PIN_7 (u8)7
+#define PIN_0 ((u8)0)
+#define PIN_1 ((u8)1)
+#define PIN_2 ((u8)2)
+#define PIN_3 ((u8)3)
+#define PIN_4 ((u8)4)
+#define PIN_5 ((u8)5)
+#define PIN_6 ((u8)6)
+#define PIN_7 ((u8)7)
 
-/*
- * macros to be used in mode param
- */
-#define OUTPUT (u8)1
-#define INPUT  (u8)0
+#define OUTPUT ((u8)1)
+#define INPUT  ((u8)0)
+#define HIGH ((u8)1)
+#define LOW  ((u8)0)
+#define Enabled ((u8)1)
+#define Disabled ((u8)0)
 
-/*
- * macros to be used in value param
- */
-#define HIGH (u8)1
-#define LOW  (u8)0
+#define E_OK ((u8)0)
+#define E_NOK ((u8)1)
 
-/*
- * macros to be used in pullup mode param
- */
-#define Enabled (u8)1
-#define Disabled  (u8)0
+u8 DIO_u8SetPinMode(u8 port_id, u8 pin_no, u8 mode);
+u8 DIO_u8SetPinValue(u8 port_id, u8 pin_no, u8 value);
+u8 DIO_u8GetPinValue(u8 port_id, u8 pin_no, u8* value);
+u8 DIO_u8IsPinOutPut(u8 port_id, u8 pin_no, u8* value);
+u8 DIO_u8SetPullUpMode(u8 port_id, u8 pin_no, u8 mode);
+u8 DIO_u8SetPortMode(u8 port_id, u8 mode);
+u8 DIO_u8SetPortValue(u8 port_id, u8 value);
+u8 DIO_u8GetPortValue(u8 port_id, u8* value);
+u8 DIO_u8SetPortPullUpMode(u8 port_id, u8 mode);
 
-/*
- * error messages
- */
-#define E_OK (u8)0
-#define E_NOK (u8)1
-
-uint8_t DIO_u8SetPinMode(u8 port_id,u8 pin_no,u8 mode);
-uint8_t DIO_u8SetPinValue(u8 port_id,u8 pin_no,u8 value);
-uint8_t DIO_u8GetPinValue(u8 port_id,u8 pin_no,u8 * value);
-uint8_t DIO_u8IsPinOutPut(u8 port_id,u8 pin_no,u8 * value);
-uint8_t DIO_u8SetPullUpMode(u8 port_id,u8 pin_no,u8 mode);
-
-uint8_t DIO_u8SetPortMode(u8 port_id,u8 mode);
-uint8_t DIO_u8SetPortValue(u8 port_id,u8 value);
-uint8_t DIO_u8GetPortValue(u8 port_id,u8 * value);
-uint8_t DIO_u8SetPortPullUpMode(u8 port_id,u8 mode);
-
-#endif /* MCAL_DIO_DIO_INTERFACE_H_ */
+#endif
