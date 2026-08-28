3b4d438b7a258c8bb9d7e307db39624c812d4455
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-08-28 05:40:40 +0300

    Fix standard type definitions and NULL

--- Speaking_System_For_Mute_People/LIB/STD_Types.h
+++ Speaking_System_For_Mute_People/LIB/STD_Types.h
@@ -1,40 +1,33 @@
-/*
- * STD_Types.h
- *
- *  Created on: Aug 26, 2026
- *      Author: DELL
- */
-
 #ifndef LIB_STD_TYPES_H_
 #define LIB_STD_TYPES_H_
 
-typedef unsigned char           u8 ;
-typedef unsigned short int      u16;
-typedef unsigned long int       u32;
-typedef unsigned long long int  u64;
-typedef signed char	            s8 ;
-typedef signed short int        s16;
-typedef signed long int         s32;
-typedef signed long long int    s64;
-typedef float                   f32;
-typedef double                  f64;
+typedef unsigned char       u8;
+typedef unsigned short      u16;
+typedef unsigned long       u32;
+typedef unsigned long long  u64;
+typedef signed char         s8;
+typedef signed short        s16;
+typedef signed long         s32;
+typedef signed long long    s64;
+typedef float               f32;
+typedef double              f64;
+
 typedef enum
-	{
-		fasle,
-		true,
-	}bool;
+{
+    false = 0,
+    true = 1
+} bool;
 
-#define NULL 0
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
 
-/* Func States  */
-#define IDLE 			0
-#define BUSY			1
-
-/* My States  */
-#define OK           	0
-#define NOK          	1
-#define NULL_POINTER 	2
-#define BUSY_STATE	 	3
-#define TIMEOUT_STATE   4
+#define IDLE 0
+#define BUSY 1
+#define OK 0
+#define NOK 1
+#define NULL_POINTER 2
+#define BUSY_STATE 3
+#define TIMEOUT_STATE 4
 
 #endif /* LIB_STD_TYPES_H_ */
