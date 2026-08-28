0e0af6528c10961c651283d678bce319f9863bf7
nouramr14 <87040699+nouramr14@users.noreply.github.com>
2026-08-28 05:43:12 +0300

    Fix tilt sensor interface types

--- Speaking_System_For_Mute_People/HAL/TILT_SENSOR/tilt_sensor_int.h
+++ Speaking_System_For_Mute_People/HAL/TILT_SENSOR/tilt_sensor_int.h
@@ -1,28 +1,20 @@
-/*
- * tilt_sensor_int.h
- *
- *  Created on: Aug 27, 2026
- *      Author: DELL
- */
-
 #ifndef HAL_TILT_SENSOR_TILT_SENSOR_INT_H_
 #define HAL_TILT_SENSOR_TILT_SENSOR_INT_H_
-/*tilt sensors to use with the tilt_pin param*/
-#define TILT_0	(uint8_t)0
-#define TILT_1	(uint8_t)1
-#define TILT_2	(uint8_t)2
-#define TILT_3	(uint8_t)3
-#define TILT_4	(uint8_t)4
-#define TILT_5	(uint8_t)5
-#define TILT_6	(uint8_t)6
-#define TILT_7	(uint8_t)7
-#define ALL_TILT (uint8_t)8
 
-void TILT_vidInit(uint8_t tilt_pin);
+#include "../../LIB/STD_Types.h"
 
-void TILT_vidGetTiltRead(uint8_t * tilt_read, uint8_t tilt_pin);
+#define TILT_0 ((u8)0)
+#define TILT_1 ((u8)1)
+#define TILT_2 ((u8)2)
+#define TILT_3 ((u8)3)
+#define TILT_4 ((u8)4)
+#define TILT_5 ((u8)5)
+#define TILT_6 ((u8)6)
+#define TILT_7 ((u8)7)
+#define ALL_TILT ((u8)8)
 
-void TILT_vidGetAllTiltRead(uint8_t * tilt_read);
+void TILT_vidInit(u8 tilt_pin);
+void TILT_vidGetTiltRead(u8* tilt_read, u8 tilt_pin);
+void TILT_vidGetAllTiltRead(u8* tilt_read);
 
-
-#endif /* HAL_TILT_SENSOR_TILT_SENSOR_INT_H_ */
+#endif
